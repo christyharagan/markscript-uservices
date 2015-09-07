515490d1196f63471d4ddbfcbda086f1653f1ac8
Christy Haragan <christy@christy.haragan@gmail.com>
2015-09-07 10:47:51 +0100

    Initial commit

---
+++ markscript-uservices.d.ts
@@ -0,0 +1,45 @@
+
+declare module "markscript-uservices/dist/lib/decorators"{
+    import tmp = require('dist/lib/decorators');
+    export = tmp;
+}
+
+declare module "markscript-uservices/dist/lib/generator"{
+    import tmp = require('dist/lib/generator');
+    export = tmp;
+}
+
+declare module "markscript-uservices/dist/lib/index"{
+    import tmp = require('dist/lib/index');
+    export = tmp;
+}
+
+declare module "markscript-uservices/dist/lib/localProxy"{
+    import tmp = require('dist/lib/localProxy');
+    export = tmp;
+}
+
+declare module "markscript-uservices/dist/lib/markscriptPlugin"{
+    import tmp = require('dist/lib/markscriptPlugin');
+    export = tmp;
+}
+
+declare module "markscript-uservices/dist/lib/model"{
+    import tmp = require('dist/lib/model');
+    export = tmp;
+}
+
+declare module "markscript-uservices/dist/lib/remoteProxy"{
+    import tmp = require('dist/lib/remoteProxy');
+    export = tmp;
+}
+
+declare module "markscript-uservices/dist/lib/rfp"{
+    import tmp = require('dist/lib/rfp');
+    export = tmp;
+}
+
+declare module "markscript-uservices"{
+    import tmp = require('dist/lib/index');
+    export = tmp;
+}
