4f44003542e546a2ce058b34fc0aae3c034db5bd
Christy Haragan <christy@christy.haragan@gmail.com>
2015-10-12 10:41:54 +0100

    Split out server-side code

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,7 +1,5 @@
 export * from './decorators'
 export * from './generator'
-export * from './localProxy'
 export * from './markscriptPlugin'
 export * from './model'
 export * from './remoteProxy'
-export * from './rfp'
