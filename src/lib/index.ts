628d64f4995e071f4b9e26bcf04c278549b442e6
Christy Haragan <christy.haragan.github@gmail.com>
2015-10-20 12:14:26 +0100

    Refactor markscript-uservices into runtime and server

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,3 @@
 export * from './decorators'
-export * from './generator'
-export * from './markscriptPlugin'
-export * from './model'
-export * from './remoteProxy'
+export * from './localProxy'
+export * from './rfp'
